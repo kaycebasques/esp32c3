53739832d2276d3834f1004bb700dd5ad281ed87
Kayce Basques <kaycebasques@gmail.com>
2026-06-17 15:40:11 -0700

    Start ssd1306 template

---
+++ templates/ssd1306/src/bin/main.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+use log::{error, info};
+
+#[panic_handler]
+fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+    error!("{}", panic_info);
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    // generator version: 1.3.0
+    // generator parameters: --chip esp32c3 -o esp32c3-wroom-02 -o log -o wokwi -o vscode
+
+    esp_println::logger::init_logger_from_env();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    // The following pins are used to bootstrap the chip. They are available
+    // for use, but check the datasheet of the module for more information on them.
+    // - GPIO2
+    // - GPIO8
+    // - GPIO9
+    // These GPIO pins are in use by some feature of the module and should not be used.
+    let _ = peripherals.GPIO11;
+    let _ = peripherals.GPIO12;
+    let _ = peripherals.GPIO13;
+    let _ = peripherals.GPIO14;
+    let _ = peripherals.GPIO15;
+    let _ = peripherals.GPIO16;
+    let _ = peripherals.GPIO17;
+
+    loop {
+        info!("Hello world!");
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(1000) {}
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
+}
